1ccfc26bc5ab1ac58255900357ac95990def8f2d
Jericho Sharman <jsharman@hotmail.com.au>
2022-05-25 00:36:10 +1000

    cre

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template,session,request,redirect
 from dotenv import load_dotenv,dotenv_values
+import psycopg2
+#from psycopg2.extras import RealDictCursor
 import os
 app = Flask(__name__)
 load_dotenv() 
 app.secret_key = os.getenv("SECRET_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
-print(SECRET_KEY)
+#print("The secret key = ",SECRET_KEY)
 
 @app.route('/')
 def index():
